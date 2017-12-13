c8b775e9303929c2e352e2b32afde92590236695
manalirupji <manali.rupji@emory.edu>
2017-12-13 15:29:27 -0500

    Add files via upload

--- functions/consensus_clustering.R
+++ functions/consensus_clustering.R
@@ -6,7 +6,7 @@ consensus_clustering <- function(dinput, mK, rep, pI, pF, cAlg, dist, iL, fL) {
   ## GETTING GENE MEDIAN CENTER D USING PEARSON CORRLN DISTANCE FOR ConsensusClusteringPlus
   data4 <-  sweep(data3, 1, apply(data3, 1, median, na.rm = T))
   
-  results = ConsensusClusterPlus(d=data4,maxK=mK,reps=rep,pItem=pI,pFeature=pF,clusterAlg=cAlg,distance=dist,innerLinkage= iL,  finalLinkage= fL, seed=1262118388.71279)
+  results = ConsensusClusterPlus(d=data4,maxK=mK, reps=rep,pItem=pI,pFeature=pF,clusterAlg=cAlg,distance=dist,innerLinkage= iL,  finalLinkage= fL, seed=1262118388.71279)
   
  return(list(output= results, data= data4, distance=dist))
   
